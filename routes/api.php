6b65cf00e65d49fc24a98990c5e2aec1a7159cfb
yuriolive <yuriso1994@gmail.com>
2019-02-21 00:09:23 -0300

    Fixing Cors problems

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,8 @@ use Illuminate\Http\Request;
 */
 
 // CORS
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
