d454b239951de25e8670ce30c8b0a7fe5739ccd1
yuriolive <yuriso1994@gmail.com>
2019-02-20 23:50:38 -0300

    Cors modification

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,12 @@ use Illuminate\Http\Request;
 */
 
 // CORS
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Credentials: true');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
